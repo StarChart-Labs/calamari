399513c2388fd87e21e6cc1b3ff26b6bc9a48141
Ryan O'Meara <romeara@live.com>
2018-10-25 21:50:37 -0400

    Add package-info documentation files

---
+++ calamari-core/src/main/java/org/starchartlabs/calamari/core/exception/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2018 StarChart Labs Authors.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+/**
+ * Typed exceptions for potential errors when interacting with GitHub
+ *
+ * @author romeara
+ */
+@ParametersAreNonnullByDefault
+package org.starchartlabs.calamari.core.exception;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ calamari-core/src/main/java/org/starchartlabs/calamari/core/webhook/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2018 StarChart Labs Authors.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+/**
+ * Utilities for interacting with GitHub webhook events
+ *
+ * @author romeara
+ */
+@ParametersAreNonnullByDefault
+package org.starchartlabs.calamari.core.webhook;
+
+import javax.annotation.ParametersAreNonnullByDefault;
